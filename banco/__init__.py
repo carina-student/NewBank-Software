1496d5288ca70080645137e0d67c4c1342f68eae
Gabriel Santana Morais <gabrielsantanamorais891@gmail.com>
2026-06-09 21:45:44 -0400

    docs: adiciona CalculadoraJuros no modulo banco

---
+++ banco/__init__.py
@@ -0,0 +1,11 @@
+from banco.conta import ContaBancaria
+from banco.extrato import Extrato
+from banco.transferencia import Transferencia
+from banco.calculadora_juros import CalculadoraJuros
+
+__all__ = [
+    'ContaBancaria',
+    'Extrato',
+    'Transferencia',
+    'CalculadoraJuros'
+]
